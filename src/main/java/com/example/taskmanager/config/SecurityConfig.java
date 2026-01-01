b957e756e200b9a9801c3c43c8496676a32e5791
Adis-cmd <priv4484@gmail.com>
2026-01-01 17:01:28 +0500

    feat(config): add default security config with BCryptPasswordEncoder

---
+++ src/main/java/com/example/taskmanager/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.taskmanager.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                )
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+                        .failureUrl("/auth/login?error=true")
+                        .usernameParameter("email")
+                        .defaultSuccessUrl("/profile", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/auth/login?logout")
+                        .permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(exception -> exception
+                        .accessDeniedPage("/errors/error"))
+                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(
+//                                "/auth/**", "/css/**", "/js/**", "/images/**"
+//                        ).permitAll()
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
