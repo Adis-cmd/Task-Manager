30710f57c6699fb5d343b85f5909374b8e663689
Adis-cmd <priv4484@gmail.com>
2026-01-02 18:55:56 +0500

    feat(util): add helper for displaying locally stored photos

--- src/main/java/com/example/taskmanager/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/taskmanager/service/impl/UserServiceImpl.java
@@ -1,22 +1,35 @@
 package com.example.taskmanager.service.impl;
 
 import com.example.taskmanager.dto.AuthRegisterDto;
+import com.example.taskmanager.dto.ProjectDto;
+import com.example.taskmanager.dto.UserDto;
+import com.example.taskmanager.dto.ViewProfileDto;
+import com.example.taskmanager.entity.ProjectMember;
 import com.example.taskmanager.entity.User;
+import com.example.taskmanager.entity.project.Project;
 import com.example.taskmanager.exception.UserNotFoundException;
 import com.example.taskmanager.repo.UserRepository;
 import com.example.taskmanager.service.AuthorityService;
+import com.example.taskmanager.service.ProjectMemberService;
 import com.example.taskmanager.service.UserService;
+import com.example.taskmanager.util.FileUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthorityService authorityService;
-
+    private final ProjectMemberService projectMemberService;
 
     @Override
     public void saveUser(AuthRegisterDto dto) {
@@ -36,4 +49,13 @@ public class UserServiceImpl implements UserService {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
     }
+
+
+    @Override
+    public ResponseEntity<?> findByName(String imageName) {
+        log.info("Получение файла изображения с именем: {}", imageName);
+        return FileUtil.getOutputFile(imageName, "images/");
+    }
+
+
 }
