5761dee59fdd4545c0c2933f5b76004348a3427a
Adis-cmd <priv4484@gmail.com>
2026-01-02 19:00:06 +0500

    feat(project): add service and repository for Project entity

---
+++ src/main/java/com/example/taskmanager/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.taskmanager.service.impl;
+
+import com.example.taskmanager.service.ProjectService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+}
