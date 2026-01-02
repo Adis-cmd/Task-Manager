58aece953562f21434019c25b23f3de0b3ed0684
Adis-cmd <priv4484@gmail.com>
2026-01-02 14:14:50 +0500

    chore(db): seed authority table with initial data

--- src/main/resources/db/changelog/initial/003_create_table_users.sql
+++ src/main/resources/db/changelog/initial/003_create_table_users.sql
@@ -1,11 +1,10 @@
 -- changelog Adis: 003 create user table
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
-    email VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
     password VARCHAR(255),
     enabled BOOLEAN,
     name VARCHAR(255),
-    user_name VARCHAR(255),
     avatar VARCHAR(255),
     authorities_id BIGINT,
     CONSTRAINT fk_authority
