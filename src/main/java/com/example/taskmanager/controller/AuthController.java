deef6272b596d075cab9fa07c650f71eac198137
Adis-cmd <priv4484@gmail.com>
2026-01-02 14:16:40 +0500

    feat(auth): add login and registration controller

---
+++ src/main/java/com/example/taskmanager/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.example.taskmanager.controller;
+
+import com.example.taskmanager.dto.AuthRegisterDto;
+import com.example.taskmanager.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+    @GetMapping("login")
+    public String login() {
+        return "auth/login";
+    }
+
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("userDto", new AuthRegisterDto());
+        return "auth/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("userDto") AuthRegisterDto userDto,
+                           BindingResult bindingResult,
+                           Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "auth/register";
+        }
+        userService.saveUser(userDto);
+        return "redirect:/auth/login";
+    }
+}
