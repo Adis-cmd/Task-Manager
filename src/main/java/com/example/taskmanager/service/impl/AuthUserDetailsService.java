d8ba146d600408682efe1d34f98cd08e8a8cf24e
Adis-cmd <priv4484@gmail.com>
2026-01-02 14:24:09 +0500

    feat(auth): add AuthUserDetailsService for Spring Security

---
+++ src/main/java/com/example/taskmanager/service/impl/AuthUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.example.taskmanager.service.impl;
+
+import com.example.taskmanager.entity.User;
+import com.example.taskmanager.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByEmail(username);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getEnabled(),
+                true,
+                true,
+                true,
+                Collections.singleton(new SimpleGrantedAuthority(user.getAuthority().getName()))
+        );
+    }
+}
