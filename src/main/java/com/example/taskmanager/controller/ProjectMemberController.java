37a11145eb57cae4082c609fab7534283ac6396f
Adis-cmd <piv4484@gmail.com>
2026-01-03 21:09:23 +0500

    feat(project-member): add controller to add user to project

---
+++ src/main/java/com/example/taskmanager/controller/ProjectMemberController.java
@@ -0,0 +1,40 @@
+package com.example.taskmanager.controller;
+
+import com.example.taskmanager.dto.AddProjectMemberRequest;
+import com.example.taskmanager.exception.ProjectNotFountException;
+import com.example.taskmanager.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/project/member")
+@RequiredArgsConstructor
+public class ProjectMemberController {
+
+    private final ProjectService projectService;
+
+    @PostMapping("/add")
+    public String addMemberForProject(
+            @ModelAttribute AddProjectMemberRequest request,
+            Principal principal,
+            RedirectAttributes redirectAttributes) {
+        try {
+            projectService.addMemberForProject(request, principal.getName());
+            redirectAttributes.addFlashAttribute("successMessage", "Участник успешно добавлен");
+        } catch (ProjectNotFountException ex) {
+            redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
+        } catch (Exception ex) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Произошла ошибка: " + ex.getMessage());
+        }
+
+        return "redirect:/project/" + request.getProjectId();
+    }
+}
