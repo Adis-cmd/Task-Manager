344e9342f1e2508f2224925a8fc9dcd6ac9e4c8a
Adis-cmd <piv4484@gmail.com>
2026-01-05 17:13:00 +0500

    feat(board): add endpoint for detailed board view

--- src/main/java/com/example/taskmanager/controller/BoardController.java
+++ src/main/java/com/example/taskmanager/controller/BoardController.java
@@ -5,6 +5,8 @@ import com.example.taskmanager.exception.ProjectNotFountException;
 import com.example.taskmanager.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,13 @@ public class BoardController {
             redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
         }
         return "redirect:/project/" + id;
+    }
+
+
+    @GetMapping("{id}")
+    public String showBoard(@PathVariable("id") Long id, Model model) {
+
+        model.addAttribute("board", boardService)
+        return "board/detail";
     }
 }
