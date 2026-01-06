a2cc5d69f51594634a6b3eafdcd2b13223de565c
Adis-cmd <piv4484@gmail.com>
2026-01-06 23:24:05 +0500

    feat(task): implement getTaskByBoardColumnId with mapper

--- src/main/java/com/example/taskmanager/service/TaskService.java
+++ src/main/java/com/example/taskmanager/service/TaskService.java
@@ -1,4 +1,7 @@
 package com.example.taskmanager.service;
 
+import com.example.taskmanager.dto.TaskDto;
+
 public interface TaskService {
+    TaskDto getTaskByBoardColumnId(Long id);
 }
--- src/main/java/com/example/taskmanager/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/taskmanager/service/impl/TaskServiceImpl.java
@@ -3,8 +3,10 @@ package com.example.taskmanager.service.impl;
 import com.example.taskmanager.dto.TaskDto;
 import com.example.taskmanager.entity.project.Task;
 import com.example.taskmanager.exception.TaskNotFoundException;
+import com.example.taskmanager.mapper.TaskMapper;
 import com.example.taskmanager.repo.TaskRepository;
 import com.example.taskmanager.service.TaskService;
+import com.example.taskmanager.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,23 +14,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class TaskServiceImpl implements TaskService {
     private final TaskRepository repository;
-
+    private final UserService userService;
+    private final TaskMapper taskMapper;
 
 
 
     @Override
     public TaskDto getTaskByBoardColumnId(Long id) {
-        Task task = repository.findTaskByColumId(id).orElseThrow(()-> new TaskNotFoundException("Task Not found"));
-
-        return TaskDto.builder()
-                .name(task.getName())
-                .description(task.getDescription())
-                .author(null)
-                .status(task.getStatus())
-                .priority(task.getPriority())
-                .participants(null)
-                .comments(null)
-                .build();
+        Task task = repository.findTaskByColumId(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task Not found"));
+        return taskMapper.toDto(task);
     }
 
 }
