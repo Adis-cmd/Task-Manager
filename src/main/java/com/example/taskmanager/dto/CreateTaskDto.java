f803997558ea78e0d0accd86e4e1d0f89d2d195c
Adis-cmd <piv4484@gmail.com>
2026-01-07 23:00:40 +0500

    feat(task): add DTO for task creation

---
+++ src/main/java/com/example/taskmanager/dto/CreateTaskDto.java
@@ -0,0 +1,26 @@
+package com.example.taskmanager.dto;
+
+import com.example.taskmanager.entity.enums.Priority;
+import com.example.taskmanager.entity.enums.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class CreateTaskDto {
+    @NotBlank
+    private String name;
+
+    private Status status;
+
+    @NotNull
+    private Priority priority;
+
+    private String description;
+}
