d0bfd70fc576e20906c5d904e97c5960cdfb87ae
Adis-cmd <piv4484@gmail.com>
2026-01-07 23:03:23 +0500

    feat(board-column): implement board column retrieval by board id

--- src/main/java/com/example/taskmanager/repo/BoardColumnRepository.java
+++ src/main/java/com/example/taskmanager/repo/BoardColumnRepository.java
@@ -2,6 +2,14 @@ package com.example.taskmanager.repo;
 
 import com.example.taskmanager.entity.project.BoardColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BoardColumnRepository extends JpaRepository<BoardColumn, Long> {
+
+
+    @Query("select c from BoardColumn c where c.board.id = :id")
+    List<BoardColumn> findByBoardId(Long id);
 }
